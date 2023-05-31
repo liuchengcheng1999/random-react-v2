cf5ec1ca6b1b3c3339f468884681cbcddccbd61f
liuchengcheng1999 <liuchengcheng233@gmail.com>
2023-05-31 11:48:13 +0800

    Updates

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/sexbird/Documents/object/random-react-v2/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
